0eb4bb74b9ea63a319e34c7b7523724d0461d25b
Tessa Thornberry <tthornberryclass@gmail.com>
2022-09-24 12:06:48 -0700

    include changes to parse.js for chatterbox server

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -1,8 +1,6 @@
 var Parse = {
 
-  // server: `http://127.0.0.1:8080/classes/messages`,
   server: 'http://127.0.0.1:3000/classes/messages',
-
 
   create: function(message, successCB, errorCB = null) {
 
